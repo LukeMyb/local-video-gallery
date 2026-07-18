4ce7194939c17cd9a692694c26199833b8a9c7bf
LukeMyb <ryutten@icloud.com>
2026-07-18 11:43:31 +0900

    refactor: App.jsxの以前のテストコードの残骸（接続情報などの状態管理変数）を削除

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Player from './pages/Player';
 
-// 接続情報
-const JELLYFIN_URL = "http://localhost:8096"; 
-const API_KEY = "053bd9021a7c4c41b60f937454f1200a";
-const TEST_VIDEO_ID = "b010a411eef74bcef0b5868532ccea7c";
-
 function App() {
-  const videoUrl = `${JELLYFIN_URL}/Videos/${TEST_VIDEO_ID}/stream?api_key=${API_KEY}`;
-
   return (
     <BrowserRouter>
       <Routes>
